d62290c3faba11c5c19d8c90cbe635fb97d86d4e
namekeptanonymous <abdulwahab3703@gmail.com>
2023-12-04 21:56:44 -0800

    Added base ViewPostFragment

---
+++ app/src/main/java/com/namekept/cosc341project/ViewPostFragment.java
@@ -0,0 +1,63 @@
+package com.namekept.cosc341project;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ViewPostFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_r_t_d, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private OnCompleteListener<DataSnapshot> onValuesFetched = new
+            OnCompleteListener<DataSnapshot>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task)
+                {
+                    if (!task.isSuccessful())
+                    {
+                        Log.e("firebase", "Error getting data.", task.getException());
+                    }
+                    else
+                    {
+                        HashMap<String, HashMap<String, String>> databaseEntries = (HashMap) task.getResult().getValue();
+                        if (databaseEntries==null)
+                            return;
+                        List<Map.Entry<String, HashMap<String, String>>> indexableEntries = new ArrayList<>(databaseEntries.entrySet());
+                    }
+                }
+            };
+}
